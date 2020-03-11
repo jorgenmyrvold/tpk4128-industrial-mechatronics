43516c410b6d348541ad261523ef21c69a7e622d
jorgenmyrvold <jorgeum@stud.ntnu.no>
2020-03-11 12:53:20 +0100

    Built working webcam on computer. Problems with Qt on my mac, but works on other computers. Problems with socket communication

--- scripts/run_client_opencv.py
+++ scripts/run_client_opencv.py
@@ -11,12 +11,12 @@ def main():
         client.sendall(b'Hello World!')
 
         # Tip: len(img.tostring())
-        size, data = client.recv(INSERT THE CORRECT IMAGE SIZE HERE)
+        size, data = client.recv(2764800)
         if not data:
             break
 
         # Tip: img.dtype, img.shape
-        img = np.frombuffer(data, INSERT THE CORRECT DATA TYPE HERE).reshape(INSERT THE CORRECT IMAGE SHAPE HERE)
+        img = np.frombuffer(data, int).reshape(720, 1280, 3)
 
         cv2.imshow('img', img)
         if cv2.waitKey(20) == 27:  # Esc: 27
--- tpk4128/camera_opencv.py
+++ tpk4128/camera_opencv.py
@@ -7,14 +7,33 @@ class Camera(object):
     def __init__(self):
 
         # Implement this constructor that opens a webcam and stores it in self._camera
-        self._camera =
+        self._camera = cv.VideoCapture(0)
+        if not self._camera.isOpened():
+            print("Not able to open camera")
+            exit()
+
 
     def capture(self):
 
         # Implement this function that grabs an image from the webcam and returns a numpy array
-
+        ret, img = self._camera.read()
+        if not ret:
+            print("Error reading from camera")
+            
         return img
 
     def __del__(self):
 
         # Implement this destructor. Remember to free the camera.
+        self._camera.release()
+        cv.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    cam = Camera()
+    while True:
+        img = cam.capture()
+        print(len(img.tostring()), img.dtype, img.shape)
+        cv.imshow("image", img)
+        cv.waitKey(1)
+
